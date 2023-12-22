c59207f521780a141fff6c05082e4cc9d4eabc2e
Shirasawa <764798966@qq.com>
2023-12-22 20:36:56 +0800

    update

--- src/main.py
+++ src/main.py
@@ -1,18 +1,22 @@
+import sys
 import musicpy.algorithms as algorithms
 import musicpy.structures as structures
 
-while True:
-    chord_notes, chord_inds = algorithms.chord_analysis(
-        structures.chord(input(), list(map(float, input().split(','))), list(map(float, input().split(',')))), 
-        is_chord=True, mode='chords', get_original_order=True
-    )
-    result = [algorithms.detect(each) for each in [chord_notes[k[0]:k[1]] for k in chord_inds]]
-    for i in result:
-        print(i if not isinstance(i, list) else i[0], end=',')
-    print(flush=False)
-    inds = [[i[0], i[1]] for i in chord_inds]
-    for k in inds:
-        bar_range = chord_notes.count_bars(k[0], k[1])
-        print(bar_range[0], end=',')
-        print(bar_range[1], end=',')
-    print()
+if __name__ == '__main__' and len(sys.argv) > 1:
+    action = sys.argv[1]
+    if action == "chords":
+        while True:
+            chord_notes, chord_inds = algorithms.chord_analysis(
+                structures.chord(input(), list(map(float, input().split(','))), list(map(float, input().split(',')))),
+                is_chord=True, mode='chords', get_original_order=True
+            )
+            result = [algorithms.detect(each) for each in [chord_notes[k[0]:k[1]] for k in chord_inds]]
+            for i in result:
+                print(i if not isinstance(i, list) else i[0], end=',')
+            print(flush=False)
+            inds = [[i[0], i[1]] for i in chord_inds]
+            for k in inds:
+                bar_range = chord_notes.count_bars(k[0], k[1])
+                print(bar_range[0], end=',')
+                print(bar_range[1], end=',')
+            print()
