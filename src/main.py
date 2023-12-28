06102c022c309afe739d25de72aef48527c59c23
Shirasawa <764798966@qq.com>
2023-12-29 02:21:55 +0800

    Update

--- src/main.py
+++ src/main.py
@@ -11,11 +11,11 @@ if __name__ == '__main__':
             )
             result = [algorithms.detect(each) for each in [chord_notes[k[0]:k[1]] for k in chord_inds]]
             for i in result:
-                print(i if not isinstance(i, list) else i[0], end=',')
-            print()
+                print(i if not isinstance(i, list) else i[0], end=',', flush=False)
+            print(flush=False)
             inds = [[i[0], i[1]] for i in chord_inds]
             for k in inds:
                 bar_range = chord_notes.count_bars(k[0], k[1])
-                print(bar_range[0], end=',')
-                print(bar_range[1], end=',')
+                print(bar_range[0], end=',', flush=False)
+                print(bar_range[1], end=',', flush=False)
             print(flush=True)
